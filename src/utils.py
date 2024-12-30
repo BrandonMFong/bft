49e1cc8184a2e059b69cc45a0e348e6e46a58178
Brando <fong.m.brandon97@gmail.com>
2024-12-30 15:55:26 -0800

    using a function to fetch pool directory

--- src/utils.py
+++ src/utils.py
@@ -7,3 +7,6 @@ import os
 SCRIPT_NAME = os.path.basename(sys.argv[0])
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
+def get_pool_dir():
+    return os.path.join(SCRIPT_PATH, "../pool")
+
