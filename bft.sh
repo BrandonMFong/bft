837c92db389dfd1ebfce19b1dadb43db158a2f12
brando <fong.m.brandon97@gmail.com>
2025-01-03 15:10:20 -0800

    Fixes sourcing for zshell

--- bft.sh
+++ bft.sh
@@ -3,7 +3,11 @@
 # date: 12/30/24
 
 function bft-init() {
-	SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )	
+	if [ "$ZSH_VERSION" != "" ]; then
+		SCRIPT_PATH=$(dirname $functions_source[$0])
+	else
+		SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )	
+	fi
 	export PATH="$SCRIPT_PATH/pool/bin:$PATH";
 	export PATH="$SCRIPT_PATH/src:$PATH";
 	export PYTHONPATH="$SCRIPT_PATH/src:$PYTHONPATH";
